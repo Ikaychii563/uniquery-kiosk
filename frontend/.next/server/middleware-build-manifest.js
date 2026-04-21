9bd5946bef24104d8d4a153d98730a87ead1d0d4
TUPi <eduartekaishaleen063@gmail.com>
2026-04-21 20:29:21 +0800

    update UI / fix bug

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -25,6 +25,11 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
+    "/character": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/character.js"
+    ],
     "/share/[id]": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
